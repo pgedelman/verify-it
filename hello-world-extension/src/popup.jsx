958b38628aa89fda639a2717869fe4f2f6ea5aea
pgedelman <pgedelman@gmail.com>
2025-01-22 17:49:55 -0500

    Further Vite

---
+++ hello-world-extension/src/popup.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Popup() {
+  return <div>Hello World from React!</div>;
+}
+
+ReactDOM.render(<Popup />, document.getElementById('root'));
